c987c0c4817947ca7c52f67e11fcc38d7d552d29
sswbhandsome <1838964905@qq.com>
2024-10-01 04:01:57 +0800

    20241001-态势标绘-30张表增删改查接口

--- ruoyi-system/src/main/java/com/ruoyi/system/service/PlotIconmanagementService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/PlotIconmanagementService.java
@@ -2,6 +2,7 @@ package com.ruoyi.system.service;
 
 import com.ruoyi.system.domain.entity.PlotIconmanagement;
 import com.baomidou.mybatisplus.extension.service.IService;
+
 public interface PlotIconmanagementService extends IService<PlotIconmanagement>{
 
 
--- ruoyi-system/src/main/java/com/ruoyi/system/service/SituationPlotService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/SituationPlotService.java
@@ -2,7 +2,18 @@ package com.ruoyi.system.service;
 
 import com.ruoyi.system.domain.entity.SituationPlot;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
 public interface SituationPlotService extends IService<SituationPlot>{
+
+    void addPlot(String plotType, Object details);
+
+    void deletePlot(String plotType, String plotId);
+
+    void updatePlotDetails(String plotType, String plotId, Object details);
+
+    Object getPlotInfos(String plotType, String plotId);
 
 
 }
