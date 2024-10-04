c9ae06ff88a8935bd69e256d6fd12eb0ccc9ef88
sswbhandsome <1838964905@qq.com>
2024-10-04 19:39:34 +0800

    20241004-根据地震获取相关点线面代码接口

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SituationPlotMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SituationPlotMapper.java
@@ -3,7 +3,11 @@ package com.ruoyi.system.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruoyi.system.domain.entity.SituationPlot;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface SituationPlotMapper extends BaseMapper<SituationPlot> {
+    List<SituationPlot> getPlot(@Param("eqid") String eqid);
 }
