c5e9e530932e62099eb56a8be24feeeb4303a5f0
newzyw123 <2541883546@qq.com>
2024-10-05 11:49:39 +0800

    创建导入功能缺失的表

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/PowerSupplyInformationServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.service.impl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.system.mapper.PowerSupplyInformationMapper;
+import com.ruoyi.system.domain.entity.PowerSupplyInformation;
+import com.ruoyi.system.service.PowerSupplyInformationService;
+@Service
+public class PowerSupplyInformationServiceImpl extends ServiceImpl<PowerSupplyInformationMapper, PowerSupplyInformation> implements PowerSupplyInformationService{
+
+}
