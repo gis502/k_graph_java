28bbc4f41f91f02c5e7940bea81389ec801951cd
fthhandsome <148979829+fthhandsome@users.noreply.github.com>
2024-10-11 16:36:10 +0800

    房屋,供水,导出

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/SupplySituation.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/SupplySituation.java
@@ -1,50 +1,92 @@
 package com.ruoyi.system.domain.entity;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentStyle;
+import com.alibaba.excel.enums.poi.HorizontalAlignmentEnum;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
-    * 震后生成-灾害现场动态信息-生命线修复信息-供水情况（用户上传数据）
-    */
+ * 震后生成-灾害现场动态信息-生命线修复信息-供水情况（用户上传数据）
+ */
 @Data
 @TableName(value = "supply_situation")
 public class SupplySituation {
     @TableId(value = "uuid", type = IdType.NONE)
-    private Object uuid;
+    private String uuid;
 
     /**
      * 地震标识id
      */
     @TableField(value = "earthquake_id")
     private Integer earthquakeId;
+    /**
+     * 地震名称
+     */
+    @TableField(value = "earthquake_name")
+    @ExcelProperty({"供水情况", "地震名称"})
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
+    private String earthquakeName;
+    /**
+     * 地震时间
+     */
+    @TableField(value = "earthquake_time")
+    @ExcelProperty({"供水情况", "地震时间"})
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
+    private LocalDateTime earthquakeTime;
 
     /**
      * 震区名称
      */
     @TableField(value = "earthquake_area_name")
+    @ExcelProperty({"供水情况", "震区名称"})
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
     private String earthquakeAreaName;
+
+    /**
+     * 填报截止时间
+     */
+    @TableField(value = "report_deadline")
+    @ExcelProperty({"供水情况", "统计截止时间"})
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
+    private LocalDateTime reportDeadline;
 
     /**
      * 保障安置点供水（处）
      */
     @TableField(value = "water_supply_points")
+    @ExcelProperty({"供水情况", "保障安置点供水（处)"})
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
     private Integer waterSupplyPoints;
 
     /**
      * 集中供水工程受损（处）
      */
     @TableField(value = "centralized_water_project_damage")
+    @ExcelProperty({"供水情况", "集中供水工程受损（处)"})
+    @ColumnWidth(30)
+    @ContentStyle(horizontalAlignment = HorizontalAlignmentEnum.LEFT) // 设置为左对齐
     private Integer centralizedWaterProjectDamage;
 
-    /**
-     * 填报截止时间
-     */
-    @TableField(value = "report_deadline")
-    private Date reportDeadline;
 
     /**
      * 系统插入时间
@@ -52,15 +94,5 @@ public class SupplySituation {
     @TableField(value = "system_insert_time")
     private Date systemInsertTime;
 
-    /**
-     * 地震时间
-     */
-    @TableField(value = "earthquake_time")
-    private Date earthquakeTime;
 
-    /**
-     * 地震名称
-     */
-    @TableField(value = "earthquake_name")
-    private String earthquakeName;
 }
--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SupplySituationServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SupplySituationServiceImpl.java
@@ -1,13 +1,96 @@
 package com.ruoyi.system.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ruoyi.system.domain.bto.RequestBTO;
+import com.ruoyi.system.domain.entity.HousingSituation;
+import com.ruoyi.system.service.strategy.DataExportStrategy;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.system.domain.entity.SupplySituation;
 import com.ruoyi.system.mapper.SupplySituationMapper;
 import com.ruoyi.system.service.SupplySituationService;
-@Service
-public class SupplySituationServiceImpl extends ServiceImpl<SupplySituationMapper, SupplySituation> implements SupplySituationService{
 
+@Service
+public class SupplySituationServiceImpl
+        extends ServiceImpl<SupplySituationMapper, SupplySituation>
+        implements SupplySituationService, DataExportStrategy {
+    /**
+     * @param requestBTO
+     * @return
+     */
+    @Override
+    public IPage<SupplySituation> getPage(RequestBTO requestBTO) {
+        String requestParam = requestBTO.getRequestParams();
+        Page<SupplySituation> supplySituation = new Page<>(requestBTO.getCurrentPage(), requestBTO.getPageSize());
+        LambdaQueryWrapper<SupplySituation> wrapper = Wrappers.lambdaQuery(SupplySituation.class)
+                .like(SupplySituation::getEarthquakeName, requestParam)
+                .or()
+                .apply("CAST(earthquake_time AS TEXT) LIKE {0}", "%" + requestParam + "%")
+                .or()
+                .like(SupplySituation::getEarthquakeAreaName, requestParam)
+                .or()
+                .apply("CAST(report_deadline AS TEXT) LIKE {0}", "%" + requestParam + "%")
+                .or()
+                .apply("CAST(water_supply_points AS TEXT) LIKE {0}", "%" + requestParam + "%")
+                .or()
+                .apply("CAST(centralized_water_project_damage AS TEXT) LIKE {0}", "%" + requestParam + "%");
+
+        return this.page(supplySituation, wrapper);
+    }
+
+    /**
+     * @param requestBTO
+     * @return
+     */
+    @Override
+    public List<SupplySituation> exportExcelGetData(RequestBTO requestBTO) {
+        String[] ids = requestBTO.getIds();
+        List<SupplySituation> list;
+        if (ids == null || ids.length == 0) {
+            list = this.list().stream()
+                    .sorted(Comparator.comparing(SupplySituation::getSystemInsertTime, Comparator.nullsLast(Comparator.naturalOrder()))
+                            .reversed()).collect(Collectors.toList());
+        } else {
+            list = this.listByIds(Arrays.asList(ids));
+        }
+        return list;
+    }
+
+
+    /**
+     * @param idsList
+     * @return
+     */
+    @Override
+    public String deleteData(List<Map<String, Object>> idsList) {
+        // 假设所有的 ids 都在每个 Map 中的 "uuid" 键下，提取所有的 ids
+        List<String> ids = new ArrayList<>();
+
+        // 遍历 requestBTO 列表，提取每个 Map 中的 "uuid" 键的值
+        for (Map<String, Object> entry : idsList) {
+            if (entry.containsKey("uuid")) {
+                // 获取 "uuid" 并转换为 String 类型
+                String uuid = (String) entry.get("uuid");
+                ids.add(uuid);
+            }
+        }
+
+        // 判断是否有 ids
+        if (ids.isEmpty()) {
+            return "没有提供要删除的 UUID 列表";
+        }
+
+        // 使用 removeByIds 方法批量删除
+        this.removeByIds(ids);
+
+        return "删除成功";
+    }
 }
