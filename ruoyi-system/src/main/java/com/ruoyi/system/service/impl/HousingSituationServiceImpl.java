b1147c5f66855bb565cf853cfc7461c159e31c12
newzyw123 <2541883546@qq.com>
2024-10-11 16:08:56 +0800

    生成housing，supply

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/HousingSituationServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.service.impl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.system.domain.entity.HousingSituation;
+import com.ruoyi.system.mapper.HousingSituationMapper;
+import com.ruoyi.system.service.HousingSituationService;
+@Service
+public class HousingSituationServiceImpl extends ServiceImpl<HousingSituationMapper, HousingSituation> implements HousingSituationService{
+
+}
