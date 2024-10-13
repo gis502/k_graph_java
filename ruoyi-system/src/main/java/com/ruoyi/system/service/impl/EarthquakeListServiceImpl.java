2b6267d0e35c77a7700508e0ff98642a5ff77fcf
Smile <3039126061@qq.com>
2024-10-13 15:00:09 +0800

    20241013完善地震列表

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthquakeListServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthquakeListServiceImpl.java
@@ -1,70 +1,59 @@
 package com.ruoyi.system.service.impl;
 
-
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.ruoyi.system.domain.entity.EarthquakeList;
-import com.ruoyi.system.mapper.EarthquakeListMapper;
-import com.ruoyi.system.service.EarthquakeListService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.system.domain.entity.EarthquakeList;
+import com.ruoyi.system.mapper.EarthquakeListMapper;
+import com.ruoyi.system.service.EarthquakeListService;
+
+import javax.annotation.Resource;
+
 @Service
 public class EarthquakeListServiceImpl extends ServiceImpl<EarthquakeListMapper, EarthquakeList> implements EarthquakeListService {
 
-    @Autowired
+    @Resource
     private EarthquakeListMapper earthquakeListMapper;
 
     @Override
-    public int addEq(EarthquakeList eq) {
-        return earthquakeListMapper.addEq(eq);
-    }
-
-    @Override
-    public int deleteEq(String eqid) {
-        return earthquakeListMapper.deleteEq(eqid);
-    }
-
-    @Override
-    public int updateEq(EarthquakeList eq) {
-        return earthquakeListMapper.updateEq(eq);
-    }
-
-    @Override
-    public List<EarthquakeList> selectAllEq() {
-        return earthquakeListMapper.selectAllEq();
-    }
-
-    @Override
-    public List<EarthquakeList> selectKeyEq() {
-        return earthquakeListMapper.selectKeyEq();
-    }
-
-    @Override
-    public List<EarthquakeList> selectLatestEq() {
-        return earthquakeListMapper.selectLatestEq();
-    }
-
-    @Override
     public List<String> getExcelUploadEarthquake() {
-        // 查询所有的 earthquakeList 数据
+        // 查询所有的 EqList 数据
         // 自定义日期时间格式化器，确保显示秒
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        List<EarthquakeList> earthquakeLists = earthquakeListMapper.selectAllEq();
+        // 创建 QueryWrapper 用于排序
+        QueryWrapper<EarthquakeList> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("occurrence_time"); // 按 OccurrenceTime 字段升序排序
+
+        List<EarthquakeList> eqLists = earthquakeListMapper.selectList(queryWrapper);
 
         // 拼接 position、time、magnitude 字段
         List<String> result = new ArrayList<>();
 
-        for (EarthquakeList eq : earthquakeLists) {
-            String combined =  eq.getOccurrenceTime().format(formatter).toString().replace("T", " ")+ " "+eq.getEarthquakeName() + "  " +"震级：" +eq.getMagnitude();
-            result.add(combined);
+        for (EarthquakeList eq : eqLists) {
+            String eqid = eq.getEqid().toString();
+            String combined = eq.getOccurrenceTime().format(formatter).toString().replace("T", " ") + " " + eq.getEarthquakeName() + "  " + "震级：" + eq.getMagnitude();
+            String resultString = eqid + " - " + combined; // 使用 "-" 或其他分隔符连接
+            result.add(resultString);
         }
         return result;
     }
 
-}
 
+    public List<EarthquakeList> getEarthquakesWithinDistance(Geometry point, double distance) {
+        return earthquakeListMapper.selectWithinDistance(point, distance);
+    }
+
+    @Override
+    public List<EarthquakeList> selectAllEq() {
+        QueryWrapper<EarthquakeList> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("occurrence_time");
+        return earthquakeListMapper.selectList(queryWrapper);
+    }
+}
