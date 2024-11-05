7d0bf0149494be631a37ce116df1a8dd3bb94ea0
xyl <192130834@qq.com>
2024-11-05 22:56:27 +0800

    2024/11/5 灾情统计，宣传社会舆论模块完成

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/PublicOpinionMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/PublicOpinionMapper.java
@@ -4,7 +4,31 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruoyi.system.domain.entity.PublicOpinion;
 import com.ruoyi.system.domain.entity.RedCrossDonations;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PublicOpinionMapper extends BaseMapper<PublicOpinion> {
+    @Select("""
+        WITH RankedRecords AS (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (
+                    PARTITION BY earthquake_zone_name
+                    ORDER BY submission_deadline DESC, system_insert_time DESC
+                ) AS rn
+            FROM
+                public_opinion
+            WHERE
+                earthquake_id = #{eqid}
+        )
+        SELECT
+            *
+        FROM
+            RankedRecords
+        WHERE
+            rn = 1
+    """)
+    List<PublicOpinion> getpublicopinion(String eqid);
 }
