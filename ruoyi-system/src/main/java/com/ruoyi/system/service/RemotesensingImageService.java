b00ceebc5d1722304114e7249e43b99c093582fc
QiShu <386485194@qq.com>
2024-11-18 17:58:44 +0800

    遥感影像页面后端

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/RemotesensingImageService.java
@@ -0,0 +1,8 @@
+package com.ruoyi.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ruoyi.system.domain.entity.RemotesensingImage;
+
+public interface RemotesensingImageService extends IService<RemotesensingImage> {
+
+}
