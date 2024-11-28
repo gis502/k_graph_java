d0181da259d97fc214d853a077cb2ff441edfd0a
xiaodemos <11165536+xiaodemos@user.noreply.gitee.com>
2024-11-28 21:22:10 +0800

    修改jackson与springboot版本不兼容的问题

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/mqtt/MessageCallbackListener.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/mqtt/MessageCallbackListener.java
@@ -1,9 +1,12 @@
 package com.ruoyi.web.core.config.mqtt;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -13,13 +16,31 @@ import java.nio.charset.StandardCharsets;
  * @date 2023/12/12 17:27
  */
 @Component
-public class MessageCallbackListener implements IMqttMessageListener {
+public class MessageCallbackListener implements IMqttMessageListener, MqttCallback {
+
+    @Resource
+    private MQTTClientUtils mqttClientUtils;
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+        System.out.println(throwable.getMessage());
+        System.out.println("连接丢失，尝试重新连接...");
+
+        mqttClientUtils.connect();
+    }
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
         String messageBody = new String(message.getPayload(), StandardCharsets.UTF_8);
-        System.out.println("收到消息："+topic+", 消息内容是："+ messageBody);
+        System.out.println("收到消息：" + topic + ", 消息内容是：" + messageBody);
     }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+
 }
 
 
