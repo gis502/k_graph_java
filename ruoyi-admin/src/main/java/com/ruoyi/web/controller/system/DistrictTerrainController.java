2647f6dc925b6ef44228a0dbf4f21e3ab285200b
YQ-Bot01 <3411616574@qq.com>
2025-01-18 14:17:56 +0800

    20250117-时间轴-打包问题解决

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/DistrictTerrainController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/DistrictTerrainController.java
@@ -1,15 +1,11 @@
 package com.ruoyi.web.controller.system;
-import com.ruoyi.system.domain.entity.DistrictEconomy;
 import com.ruoyi.system.domain.entity.DistrictTerrain;
-import com.ruoyi.system.service.DistrictEconomyService;
 import com.ruoyi.system.service.DistrictTerrainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 
 //指挥大屏最下角轮播图
