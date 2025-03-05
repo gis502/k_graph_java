3de17fd04fd9cffbceed4f59e077847fba951855
xiaodemos <11165536+xiaodemos@user.noreply.gitee.com>
2025-03-05 23:59:10 +0800

    指挥大屏添加倾斜模型数据

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/Tiltphotographymodel.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/Tiltphotographymodel.java
@@ -5,12 +5,16 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import org.locationtech.jts.geom.Geometry;
+import org.n52.jackson.datatype.jts.GeometryDeserializer;
+import org.n52.jackson.datatype.jts.GeometrySerializer;
+import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -19,6 +23,13 @@ import java.util.UUID;
 @Data
 @TableName(value = "tiltphotographymodel")
 public class Tiltphotographymodel {
+
+    /**
+     * 模型id
+     */
+    @TableId(value = "uuid", type = IdType.NONE) // 使用 NONE 类型，手动生成 UUID
+    private String uuid;
+
     /**
      * 模型名称
      */
@@ -44,20 +55,6 @@ public class Tiltphotographymodel {
     private Integer tz;
 
     /**
-     * 模型添加时间
-     */
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @TableField(value = "time") // 确保格式正确
-    private LocalDateTime time; // 使用 Date 类型
-
-    /**
-     * 模型id
-     */
-    @TableId(value = "uuid", type = IdType.NONE) // 使用 NONE 类型，手动生成 UUID
-    private String uuid;
-
-    /**
      * 添加高程后，在z轴上旋转了多少度
      */
     @TableField(value = "rze")
@@ -75,10 +72,27 @@ public class Tiltphotographymodel {
     @TableField(value = "model_size")
     private Double modelSize;
 
+    @TableField(value = "geom")
+    @JsonSerialize(using = GeometrySerializer.class)
+    @JsonDeserialize(using = GeometryDeserializer.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)  // 仅序列化非空字段
+    private Geometry geom;
+
+    /**
+     * 模型添加时间
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField(value = "time") // 确保格式正确
+    private LocalDateTime time; // 使用 Date 类型
+
+
     // 在保存前检查 UUID 是否为空，如果为空则自动生成
     public void generateUuidIfNotPresent() {
         if (this.uuid == null || this.uuid.trim().isEmpty()) {
             this.uuid = UUID.randomUUID().toString();
         }
     }
+
+
 }
